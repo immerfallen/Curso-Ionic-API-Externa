9da0478a9c725a1363f89dfe2258b16f4a3dfc1e
Maro de Melo <marodemelo10@gmail.com>
2021-10-14 13:29:29 -0300

    estudando consumo de API externa com Ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'api',
+    loadChildren: () => import('./pages/api/api.module').then( m => m.ApiPageModule)
   }
 ];
 
